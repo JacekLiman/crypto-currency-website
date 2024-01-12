64e0bdf7cd265e8b326d02032f1054dcf3f75b09
Jacek Liman <limaniec@gmail.com>
2024-01-12 12:21:56 +0100

    add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import Hero from "./components/Hero";
+import { useEffect } from "react";
+
 function App() {
-  return <div className="">Hello</div>;
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+    });
+  });
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
